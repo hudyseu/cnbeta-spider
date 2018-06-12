fa024e9508a464cc45d5cff67cc2ccacf8b3ab5b
hudingyu <hudingyu@meituan.com>
2018-06-12 13:36:51 +0800

    update ip

--- config/dbconf.js
+++ config/dbconf.js
@@ -4,7 +4,7 @@
  */
 module.exports = {
     localUrl: 'localhost',
-    originIp: '172.26.8.153',
+    originIp: '172.26.8.62',
     localPort: 27017,
     db: {
         articlelist: 'articleList'
